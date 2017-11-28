8e5e49aa4e2c7759b4dd0cad9482bb95be6b7dd2
Mike Van Winkle <mvanwinkle@zendesk.com>
2017-11-28 10:25:12 -0800

    Don't use sudo on env commands

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 sudo apt-get install python2.7 python-pip
 sudo pip install virtualenv
-sudo virtualenv env
+virtualenv env
 source env/bin/activate
 export AWS_ACCESS_KEY_ID='YOURKEY'
 export AWS_ACCESS_SECRET_KEY='YOURSECRET'
 export AWS_REGION='us-west-2' # default region
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
