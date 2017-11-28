08c7a0f6eb0a1b8a5de5d7a0e24fc6a249a4a4fa
Mike Van Winkle <mvanwinkle@zendesk.com>
2017-11-28 09:53:44 -0800

    removing bloat

--- README.md
+++
@@ -1,8 +0,0 @@
-# Description
-This is a simple python script to list and sort your EC2 instances. 
-
-# Installation and Setup
-Simply clone the respository to your local machine and run `./setup.sh`
-
-
-
--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source bin/activate
-export AWS_ACCESS_KEY_ID='YOURKEY'
-export AWS_ACCESS_SECRET_KEY='YOURSECRET'
-export AWS_REGION='us-west-2' # default region 
-pip install -r requirements.txt
